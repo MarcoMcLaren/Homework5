b80599498c850350cb32e08018ea4e3d0953e7e5
Marco Mac <mclarenmarco998@gmail.com>
2022-09-27 16:03:06 +0200

    Add project files.

---
+++ Homework5/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Homework5.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Homework5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly LibraryEntities db = new LibraryEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            var books = new BooksVM
+            {
+                Authors = db.authors.ToList(),
+                Types = db.types.ToList(),
+                Books = db.books.ToList(),
+
+            };
+            return View(books);
+
+
+        }
+    }
+}
---
+++ Homework5/Models/BooksVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Homework5.Models
+{
+    public class BooksVM
+    {
+        public IEnumerable<books> Books { get; set; }
+        public IEnumerable<authors> Authors { get; set; }
+        public IEnumerable<types> Types { get; set; }
+
+    }
+}
